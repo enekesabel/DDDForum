d305e58eeefb80696d57d80af92375f73993c2fa
enekesabel <enekes.abel@gmail.com>
2024-07-31 15:11:35 +0200

    feat: implement editing user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -59,7 +59,84 @@ app.post('/users/new', async (req: Request, res: Response) => {
 
 // Edit a user
 app.post('/users/edit/:userId', async (req: Request, res: Response) => {
-  // ...
+
+  try {
+
+    const {id,password, ...userData} = req.body;
+
+    // Validate input
+    if(!isValidUser(userData)){
+      return res.status(400).json({
+        error: Errors.ValidationError,
+        data: undefined,
+        success: false
+      })
+    }
+
+    const foundUserById = await prisma.user.findUnique({
+      where: {
+        id: Number(req.params.userId)
+      }
+    })
+
+    if(!foundUserById){
+      return res.status(404).json({
+        error: Errors.UserNotFound, 
+        data: undefined, 
+        success: false 
+      })
+    }
+
+    const foundUserByEmail = await prisma.user.findUnique({
+      where: {
+        email: userData.email
+      }
+    })
+
+    if(foundUserByEmail && foundUserByEmail.id !== foundUserById.id){
+      return res.status(409).json({
+        error: Errors.EmailAlreadyInUse, 
+        data: undefined, 
+        success: false 
+      })
+    }
+
+    const foundUserByUsername = await prisma.user.findUnique({
+      where: {
+        username: userData.username
+      }
+    })
+
+    if(foundUserByUsername && foundUserByUsername.id !== foundUserById.id){
+      return res.status(409).json({
+        error: Errors.UsernameAlreadyTaken, 
+        data: undefined, 
+        success: false 
+      })
+    }
+    
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: foundUserById.id
+      },
+        data: userData
+    });
+
+    return res.status(200).json({       
+      error: undefined, 
+      data: parseUserForResponse(updatedUser), 
+      succes: true 
+    });
+
+  } catch (error) {
+    console.log(error)
+    // Return a failure error response
+    return res.status(500).json({ 
+      error: Errors.ServerError, 
+      data: undefined, 
+      success: false 
+     });
+  }
 });
 
 // Get a user by email
