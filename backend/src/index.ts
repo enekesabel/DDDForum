3245aba2bd3d4e7050a1841cf0147975a8f09ca0
enekesabel <enekes.abel@gmail.com>
2024-07-31 12:25:23 +0200

    feat: implement finding user by email

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,7 +91,41 @@ app.post('/users/edit/:userId', async (req: Request, res: Response) => {
 
 // Get a user by email
 app.get('/users', async (req: Request, res: Response) => {
-  // ...
+  try {
+    const {email} = req.query
+
+    if(!email){
+      throw new Error();
+    }
+
+    const foundUser = await prisma.user.findUnique({
+      where: {
+        email: String(email)
+      }
+    })
+
+    if(!foundUser){
+      return res.status(404).json({
+        error: Errors.UserNotFound, 
+        data: undefined, 
+        success: false 
+      })
+    }
+
+    return res.status(200).json({       
+      error: undefined, 
+      data: parseUserForResponse(foundUser), 
+      succes: true 
+    });
+  } catch (error) {
+    console.log(error)
+    // Return a failure error response
+    return res.status(500).json({ 
+      error: Errors.ServerError, 
+      data: undefined, 
+      success: false 
+     });
+  }
 });
 
 const port = process.env.PORT || 3000;
