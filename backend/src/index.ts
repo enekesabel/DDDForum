4c22629bb0628ac96318937b1d52efbd18da272f
enekesabel <enekes.abel@gmail.com>
2024-07-31 09:24:05 +0200

    feat: implement creating user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,56 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import {prisma} from './client'
+import { User } from '@prisma/client';
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-// Create a new user
+
+function generateRandomPassword(length: number): string {
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+  const passwordArray = [];
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    passwordArray.push(charset[randomIndex]);
+  }
+
+  return passwordArray.join('');
+}
+
+// We don't want to return the password within the request
+function parseUserForResponse (user: User) {
+  const returnData = JSON.parse(JSON.stringify(user));
+  delete returnData.password;
+  return returnData;
+}
+
+// Create new user
 app.post('/users/new', async (req: Request, res: Response) => {
-  // ...
+  try {
+    const userData = req.body;
+    
+    // Success case
+    const user = await prisma.user.create({ 
+      data: { ...userData, password: generateRandomPassword(10) } 
+    });
+    
+    return res.status(201).json({       
+      error: undefined, 
+      data: parseUserForResponse(user), 
+      succes: true 
+    });
+  } catch (error) {
+    console.log(error)
+    // Return a failure error response
+    return res.status(500).json({ 
+      error: 'ServerError', 
+      data: undefined, 
+      success: false 
+     });
+  }
 });
 
 // Edit a user
