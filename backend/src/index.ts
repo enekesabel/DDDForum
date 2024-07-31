6a28ab41e3031e61d59da13e839980f49c9d1e8f
enekesabel <enekes.abel@gmail.com>
2024-07-31 15:22:40 +0200

    refactor: remove unnecessary console.log

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -187,5 +187,4 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
-  console.log(`You have ${await prisma.user.count()} users.`)
 });
