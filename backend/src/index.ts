d8f24ebf1e87e4849e87898353ac360c35cd0f1e
enekesabel <enekes.abel@gmail.com>
2024-08-02 14:09:47 +0200

    chore: enable formatting with prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,123 +1,120 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import { createUser, findUserByEmail, findUserById, findUserByUsername, updateUser } from './database'
+import { createUser, findUserByEmail, findUserById, findUserByUsername, updateUser } from './database';
 import { errorResponseBuilder, generateRandomPassword, isValidUser, parseUserForResponse } from './utils';
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post('/users/new', async (req: Request, res: Response) => {
-  const errorBuilder = errorResponseBuilder(res);
+    const errorBuilder = errorResponseBuilder(res);
 
-  try {
-    const userData = req.body;
+    try {
+        const userData = req.body;
 
-    if(!isValidUser(req.body)){
-      return errorBuilder.validationError()
-    }
-    
-    const existingUserByEmail = await findUserByEmail(req.body.email);
-    if (existingUserByEmail) {
-      return errorBuilder.emailAlreadyInUse()
-    }
-    
-    const existingUserByUsername = await findUserByUsername(req.body.username);
-    if (existingUserByUsername) {
-      return errorBuilder.usernameAlreadyTaken()
-    }
+        if (!isValidUser(req.body)) {
+            return errorBuilder.validationError();
+        }
 
-    const user = await createUser({ 
-      data: { ...userData, password: generateRandomPassword(10) } 
-    });
-    
-    return res.status(201).json({       
-      error: undefined, 
-      data: parseUserForResponse(user), 
-      succes: true 
-    });
-  } catch (error) {
-    return errorBuilder.serverError()
-  }
+        const existingUserByEmail = await findUserByEmail(req.body.email);
+        if (existingUserByEmail) {
+            return errorBuilder.emailAlreadyInUse();
+        }
+
+        const existingUserByUsername = await findUserByUsername(req.body.username);
+        if (existingUserByUsername) {
+            return errorBuilder.usernameAlreadyTaken();
+        }
+
+        const user = await createUser({
+            data: { ...userData, password: generateRandomPassword(10) },
+        });
+
+        return res.status(201).json({
+            error: undefined,
+            data: parseUserForResponse(user),
+            succes: true,
+        });
+    } catch (error) {
+        return errorBuilder.serverError();
+    }
 });
 
 // Edit a user
 app.post('/users/edit/:userId', async (req: Request, res: Response) => {
+    const errorBuilder = errorResponseBuilder(res);
 
-  const errorBuilder = errorResponseBuilder(res);
+    try {
+        const { id, password, ...userData } = req.body;
 
-  try {
+        if (!isValidUser(userData)) {
+            return errorBuilder.validationError();
+        }
 
-    const {id,password, ...userData} = req.body;
+        const foundUserById = await findUserById(Number(req.params.userId));
 
-    if(!isValidUser(userData)){
-      return errorBuilder.validationError()
+        if (!foundUserById) {
+            return errorBuilder.userNotFound();
+        }
+
+        const foundUserByEmail = await findUserByEmail(userData.email);
+
+        // Allow passing the email unchanged
+        // Only throw error if we'd try to assing the same email to a different user
+        if (foundUserByEmail && foundUserByEmail.id !== foundUserById.id) {
+            return errorBuilder.emailAlreadyInUse();
+        }
+
+        const foundUserByUsername = await findUserByUsername(userData.username);
+
+        // Allow passing the username unchanged
+        // Only throw error if we'd try to assing the same username to a different user
+        if (foundUserByUsername && foundUserByUsername.id !== foundUserById.id) {
+            return errorBuilder.usernameAlreadyTaken();
+        }
+
+        const updatedUser = await updateUser(id, userData);
+
+        return res.status(200).json({
+            error: undefined,
+            data: parseUserForResponse(updatedUser),
+            succes: true,
+        });
+    } catch (error) {
+        return errorBuilder.serverError();
     }
-
-    const foundUserById = await findUserById(Number(req.params.userId))
-
-    if(!foundUserById){
-      return errorBuilder.userNotFound()
-    }
-
-    const foundUserByEmail = await findUserByEmail(userData.email)
-
-    // Allow passing the email unchanged
-    // Only throw error if we'd try to assing the same email to a different user
-    if(foundUserByEmail && foundUserByEmail.id !== foundUserById.id){
-      return errorBuilder.emailAlreadyInUse()
-    }
-
-    const foundUserByUsername = await findUserByUsername(userData.username)
-
-    // Allow passing the username unchanged
-    // Only throw error if we'd try to assing the same username to a different user
-    if(foundUserByUsername && foundUserByUsername.id !== foundUserById.id){
-      return errorBuilder.usernameAlreadyTaken()
-    }
-    
-    const updatedUser = await updateUser(id, userData)
-
-    return res.status(200).json({       
-      error: undefined, 
-      data: parseUserForResponse(updatedUser), 
-      succes: true 
-    });
-
-  } catch (error) {
-    return errorBuilder.serverError()
-  }
 });
 
 // Get a user by email
 app.get('/users', async (req: Request, res: Response) => {
-  const errorBuilder = errorResponseBuilder(res);
+    const errorBuilder = errorResponseBuilder(res);
 
-  try {
-    const {email} = req.query
+    try {
+        const { email } = req.query;
 
-    if(!email){
-      throw new Error();
+        if (!email) {
+            throw new Error();
+        }
+
+        const foundUser = await findUserByEmail(String(email));
+
+        if (!foundUser) {
+            return errorBuilder.userNotFound();
+        }
+
+        return res.status(200).json({
+            error: undefined,
+            data: parseUserForResponse(foundUser),
+            succes: true,
+        });
+    } catch (error) {
+        return errorBuilder.serverError();
     }
-
-    const foundUser = await findUserByEmail(String(email))
-
-    if(!foundUser){
-      return errorBuilder.userNotFound()
-    }
-
-    return res.status(200).json({       
-      error: undefined, 
-      data: parseUserForResponse(foundUser), 
-      succes: true 
-    });
-  } catch (error) {
-    return errorBuilder.serverError()
-  }
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,68 +1,69 @@
-import { User } from "./database";
+import { User } from './database';
 import { Response } from 'express';
 
 // We don't want to return the password within the request
 export function parseUserForResponse(user: User) {
-  const returnData = JSON.parse(JSON.stringify(user));
-  delete returnData.password;
-  return returnData
+    const returnData = JSON.parse(JSON.stringify(user));
+    delete returnData.password;
+    return returnData;
 }
 
 export function generateRandomPassword(length: number): string {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
-  const passwordArray = [];
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    const passwordArray = [];
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    passwordArray.push(charset[randomIndex]);
-  }
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        passwordArray.push(charset[randomIndex]);
+    }
 
-  return passwordArray.join('');
+    return passwordArray.join('');
 }
 
 export function isValidUser(user: User): boolean {
-  return !!user.email &&
-    !!user.username &&
-    !!user.firstName &&
-    !!user.lastName;
+    return !!user.email && !!user.username && !!user.firstName && !!user.lastName;
 }
 
 const Errors = {
-  UsernameAlreadyTaken: 'UserNameAlreadyTaken',
-  EmailAlreadyInUse: 'EmailAlreadyInUse',
-  ValidationError: 'ValidationError',
-  ServerError: 'ServerError',
-  ClientError: 'ClientError',
-  UserNotFound: 'UserNotFound'
+    UsernameAlreadyTaken: 'UserNameAlreadyTaken',
+    EmailAlreadyInUse: 'EmailAlreadyInUse',
+    ValidationError: 'ValidationError',
+    ServerError: 'ServerError',
+    ClientError: 'ClientError',
+    UserNotFound: 'UserNotFound',
 };
 
 export const errorResponseBuilder = (res: Response) => {
-  return {
-    usernameAlreadyTaken: () => res.status(409).json({
-      error: Errors.UsernameAlreadyTaken,
-      data: undefined,
-      success: false
-    }),
-    emailAlreadyInUse: () => res.status(409).json({
-      error: Errors.EmailAlreadyInUse,
-      data: undefined,
-      success: false
-    }),
-    validationError: () => res.status(400).json({
-      error: Errors.ValidationError,
-      data: undefined,
-      success: false
-    }),
-    serverError: () => res.status(500).json({
-      error: Errors.ServerError,
-      data: undefined,
-      success: false
-    }),
-    userNotFound: () => res.status(404).json({
-      error: Errors.UserNotFound,
-      data: undefined,
-      success: false
-    }),
-  };
+    return {
+        usernameAlreadyTaken: () =>
+            res.status(409).json({
+                error: Errors.UsernameAlreadyTaken,
+                data: undefined,
+                success: false,
+            }),
+        emailAlreadyInUse: () =>
+            res.status(409).json({
+                error: Errors.EmailAlreadyInUse,
+                data: undefined,
+                success: false,
+            }),
+        validationError: () =>
+            res.status(400).json({
+                error: Errors.ValidationError,
+                data: undefined,
+                success: false,
+            }),
+        serverError: () =>
+            res.status(500).json({
+                error: Errors.ServerError,
+                data: undefined,
+                success: false,
+            }),
+        userNotFound: () =>
+            res.status(404).json({
+                error: Errors.UserNotFound,
+                data: undefined,
+                success: false,
+            }),
+    };
 };
-
