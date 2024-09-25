6f270b6c93f50654921c198cc012fac0bc676753
enekesabel <enekes.abel@gmail.com>
2024-09-25 10:41:20 +0200

    chore(backend): validate env before running scripts

--- packages/backend/prepareEnv.ts
+++ packages/backend/prepareEnv.ts
@@ -11,23 +11,37 @@
  */
 
 import { execSync } from 'child_process';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import { cleanEnv, str } from 'envalid';
 import * as path from 'path';
+
+const validateEnv = (envFile: string) => {
+  cleanEnv(
+    {
+      ...dotenv.parse(fs.readFileSync(envFile)),
+    },
+    {
+      DATABASE_URL: str(),
+    }
+  );
+};
 
 export const prepareEnv = (): void => {
   const env = process.env.NODE_ENV || 'development';
   const packageRoot = path.resolve(__dirname);
-
   const execParams = {
     cwd: packageRoot,
     stdio: 'inherit',
   } as const;
-
   const script = process.argv.splice(2).join(' ');
+  const envFile = `.env.${env}`;
+  const envFileExists = fs.existsSync(envFile);
 
-  if (env === 'development') {
-    const devEnvFile = '.env.development';
-    console.log(`Preparing dev environment using ${devEnvFile}`);
-    execSync(`dotenv -e ${devEnvFile} -- ${script}`, execParams);
+  if (envFileExists) {
+    validateEnv(envFile);
+    console.log(`Preparing dev environment using ${envFile}`);
+    execSync(`dotenv -e ${envFile} -- ${script}`, execParams);
     return;
   }
 
