f5c70091330a006b556e06aed9f67ab4053fc887
enekesabel <enekes.abel@gmail.com>
2024-10-04 18:30:57 +0200

    chore(backend): set up running unit tests

--- packages/backend/jest.config.ts
+++ packages/backend/jest.config.ts
@@ -1,7 +1,10 @@
-module.exports = {
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+export default async (): Promise<JestConfigWithTsJest> => ({
+  displayName: 'Backend (Unit)',
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   testRegex: '.*\\.(test|spec)?\\.(ts|ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-};
+  roots: ['<rootDir>/src'],
+});
