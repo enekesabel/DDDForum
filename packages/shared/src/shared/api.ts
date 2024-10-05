82129cabb051dd1c04aa265bd747277e33d6a76a
enekesabel <enekes.abel@gmail.com>
2024-10-05 13:04:23 +0200

    feat(shared): create api response schemas

--- packages/shared/src/shared/api.ts
+++ packages/shared/src/shared/api.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export type APIError<U> = {
   message: string;
   code: U;
@@ -8,12 +10,27 @@ export type APISuccessResponse<T> = {
   data: T;
   error?: undefined;
 };
+export const createAPISuccessResponseSchema = <T extends z.ZodTypeAny>(data: T) =>
+  z.object({
+    success: z.literal(true),
+    data: data,
+    error: z.undefined(),
+  });
 
 export type APIErrorResponse<U> = {
   success: false;
   error: APIError<U>;
   data?: undefined;
 };
+export const createAPIErrorResponseSchema = <U extends z.ZodTypeAny>(error: U) =>
+  z.object({
+    success: z.literal(false),
+    error: error,
+    data: z.undefined(),
+  });
+
+export const createAPIResponseSchema = <D extends z.ZodTypeAny, E extends z.ZodTypeAny>(data: D, error: E) =>
+  z.discriminatedUnion('success', [createAPISuccessResponseSchema(data), createAPIErrorResponseSchema(error)]);
 
 export type APIResponse<T, U> = APISuccessResponse<T> | APIErrorResponse<U>;
 
