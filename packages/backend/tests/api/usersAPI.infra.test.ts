4892ac4c0d737d6b0ff4a8a5426c4f085f363867
enekesabel <enekes.abel@gmail.com>
2024-10-26 14:30:13 +0200

    test(backend): incoming integration test 'post /users/edit/:userId'

--- packages/backend/tests/api/usersAPI.infra.test.ts
+++ packages/backend/tests/api/usersAPI.infra.test.ts
@@ -1,6 +1,6 @@
 import { APIClient } from '@dddforum/shared/src/core';
 import { UserInputBuilder } from '@dddforum/shared/tests/support';
-import { CreateUserResponseSchema } from '@dddforum/shared/src/modules/users';
+import { CreateUserResponseSchema, UpdateUserResponseSchema } from '@dddforum/shared/src/modules/users';
 import { CompositionRoot } from '../../src/core';
 import { Config, WebServer } from '../../src/shared';
 import { buildMockAPIReponse, DatabaseFixtures } from '../support/fixtures';
@@ -20,37 +20,37 @@ describe('usersAPI', () => {
   let server: WebServer;
   let apiClient: APIClient;
 
-  let createUserSpy: jest.SpyInstance;
-
   beforeAll(async () => {
     server = composition.getWebServer();
     await server.start();
     apiClient = APIClient.FromServer(server.getServer());
-    createUserSpy = jest.spyOn(application.users, 'createUser');
-  });
-
-  afterEach(() => {
-    createUserSpy.mockClear();
   });
 
   afterAll(async () => {
     await server.stop();
   });
 
-  describe('creating users', () => {
+  describe('post /users/new', () => {
     const createUserInput = new UserInputBuilder().withAllRandomDetails().build();
+    const schema = CreateUserResponseSchema;
+    let createUserSpy: jest.SpyInstance;
+
+    beforeAll(() => {
+      createUserSpy = jest.spyOn(application.users, 'createUser');
+    });
+
+    afterEach(() => {
+      createUserSpy.mockClear();
+    });
 
     it('should be able to successfully create a user', async () => {
-      const createUserStub = await databaseFixtures.makeUserBuilder().withAllRandomDetails().build();
+      const createUserStub = await databaseFixtures.makeUserBuilder().fromUserInput(createUserInput).build();
 
       createUserSpy.mockResolvedValue(createUserStub);
 
       const response = await apiClient.users.register(createUserInput);
 
-      const expectedCreateUserResponse = buildMockAPIReponse()
-        .schema(CreateUserResponseSchema)
-        .data(createUserStub)
-        .build();
+      const expectedCreateUserResponse = buildMockAPIReponse().schema(schema).data(createUserStub).build();
 
       expect(application.users.createUser).toHaveBeenCalledTimes(1);
       expect(response).toEqual(expectedCreateUserResponse);
@@ -67,9 +67,55 @@ describe('usersAPI', () => {
     it.each(errorsToCheck)('should be able to handle %s error', async (_key, error) => {
       createUserSpy.mockRejectedValue(error);
 
-      const expectedErrorResponse = buildMockAPIReponse().schema(CreateUserResponseSchema).error(error).build();
+      const expectedErrorResponse = buildMockAPIReponse().schema(schema).error(error).build();
 
       const response = await apiClient.users.register(createUserInput);
+
+      expect(response).toEqual(expectedErrorResponse);
+    });
+  });
+
+  describe('post /users/edit/:userId', () => {
+    const updateUserInput = new UserInputBuilder().withAllRandomDetails().build();
+    const userId = 1;
+    const schema = UpdateUserResponseSchema;
+
+    let updateUserSpy: jest.SpyInstance;
+
+    beforeAll(() => {
+      updateUserSpy = jest.spyOn(application.users, 'updateUser');
+    });
+
+    afterEach(() => {
+      updateUserSpy.mockClear();
+    });
+
+    it('should be able to successfully update a user', async () => {
+      const updatedUserStub = await databaseFixtures.makeUserBuilder().fromUserInput(updateUserInput).build();
+
+      updateUserSpy.mockResolvedValue(updatedUserStub);
+
+      const response = await apiClient.users.editUser(userId, updateUserInput);
+
+      const expectedUpdateUserResponse = buildMockAPIReponse().schema(schema).data(updatedUserStub).build();
+
+      expect(application.users.updateUser).toHaveBeenCalledTimes(1);
+      expect(response).toEqual(expectedUpdateUserResponse);
+    });
+
+    const errorsToCheck = [
+      ...Object.entries(GENERIC_ERROR_EXCEPTIONS),
+      ...Object.entries(VALIDATION_ERROR_EXCEPTIONS),
+      ...Object.entries(CLIENT_ERROR_EXCEPTIONS),
+      ...Object.entries(USERS_EXCEPTIONS),
+    ] as const;
+
+    it.each(errorsToCheck)('should be able to handle %s error', async (_key, error) => {
+      updateUserSpy.mockRejectedValue(error);
+
+      const expectedErrorResponse = buildMockAPIReponse().schema(schema).error(error).build();
+
+      const response = await apiClient.users.editUser(userId, updateUserInput);
 
       expect(response).toEqual(expectedErrorResponse);
     });
